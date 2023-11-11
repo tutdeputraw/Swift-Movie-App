2b3fb43c61ac3b23170ff50e45a819675e74d339
tutdeputraw <anoboy.code@gmail.com>
2023-11-11 15:41:08 +0700

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by tutdeputraw on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
